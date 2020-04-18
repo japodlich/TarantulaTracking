acb1c57615d0a4d3320107a663bbb6804217e72b
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-04-17 19:10:38 -0500

    added code to TarantulaWebController

--- TarantulaTracking/src/main/java/dmacc/controller/TarantulaWebController.java
+++ TarantulaTracking/src/main/java/dmacc/controller/TarantulaWebController.java
@@ -1,5 +1,25 @@
 package dmacc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import dmacc.repository.TarantulaRepository;
+
+
+
+@Controller
 public class TarantulaWebController {
+
+	@Autowired
+	TarantulaRepository tRepo;
+	
+	
+	@GetMapping("/viewAll")
+	public String viewAllContacts(Model model) {
+	model.addAttribute("tarantulas", tRepo.findAll());
+	return "ListTarantulas";   //ListTarantulas.html
+	}
 
 }
