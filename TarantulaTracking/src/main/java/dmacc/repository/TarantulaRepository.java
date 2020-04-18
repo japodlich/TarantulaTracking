8411ed5a87f2bea2dbae240179775083b06b38a0
ebethLuong <52637513+ebethLuong@users.noreply.github.com>
2020-04-17 19:21:40 -0500

    wip

--- TarantulaTracking/src/main/java/dmacc/repository/TarantulaRepository.java
+++ TarantulaTracking/src/main/java/dmacc/repository/TarantulaRepository.java
@@ -1,5 +1,11 @@
 package dmacc.repository;
 
-public interface TarantulaRepository {
-//sup
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dmacc.beans.Tarantula;
+
+
+
+public interface TarantulaRepository extends JpaRepository <Tarantula, Long> {
+
 }
